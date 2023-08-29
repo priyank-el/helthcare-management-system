2d1ccdb287ab04074b183aa423011931605fd8d2
priyankambliya18@gmail.com <priyank.a@elaunchinfotech.in>
2023-08-29 13:50:45 +0530

    add patientSchema..

---
+++ models/patients.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const patientsSchema = new mongoose.Schema({
+    nickname:{
+        type:String
+    },
+    DOB:{
+        type:String
+    },
+    contact_no:{
+        type:String
+    },
+    address:{
+        type:String
+    },
+    allergies:{
+        type:Array
+    },
+    medical_history:{
+        type:String
+    },
+    current_condition:{
+        type:String
+    },
+    userId:{
+        type:mongoose.Types.ObjectId,
+        ref:'User'
+    },
+    email:{
+        type:String
+    }
+},
+{timestamps:true})
+
+const Patient = mongoose.model('Patient',patientsSchema)
+export default Patient
