aa1f5eea26806dddf6958581c579fbc4ad8f52d1
priyankambliya18@gmail.com <priyank.a@elaunchinfotech.in>
2023-09-22 16:42:06 +0530

    add file

---
+++ middleware/languageAuth.ts
@@ -0,0 +1,21 @@
+import { Request,Response } from "express";
+import { HINDI } from "../languages/hi";
+import { ENGLISH } from "../languages/en";
+import { errorResponse } from "../handler/responseHandler";
+
+const languageAuth = async (req:Request, res:Response, next:any) => {
+    try {
+        const lang = req.headers.lang;
+
+        const language = lang === 'hi' 
+        ? HINDI
+        : ENGLISH
+
+        req.body.language = language;
+        next();
+    } catch (error:any) {
+        errorResponse(res,error,401)
+    }
+}
+
+export default languageAuth
---
+++ models/admin.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const adminSchema = new mongoose.Schema({
+    name:{
+        type:String
+    },
+    email:{
+        type:String
+    },
+    password:{
+        type:String
+    },
+    image:{
+        type:String,
+        get: function(value:string) {
+            // Transform and return value here
+            return `http://localhost:4001/images/${value}`;
+        }
+    }
+});
+
+const Admin = mongoose.model('Admin',adminSchema);
+export default Admin;
